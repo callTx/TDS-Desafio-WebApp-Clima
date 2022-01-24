6a00fdc77fe7f6d635899395129324065853d408
callTx <danielfdeus@gmail.com>
2022-01-24 09:10:48 -0300

    Add um Service ao Component Localidade

---
+++ angular-webappclima/src/app/pag-localidade/weatherapi.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
+
+/**Cria anotação para classe WeatherapiService */
+/** Permite injetar a classe WeatherapiService em outras classes */
+@Injectable({
+  providedIn: 'root'
+})
+/* Expõe a classe WeatherapiService para todos os arquivos da Aplicação*/
+export class WeatherapiService {
+  weather_data: any;
+  private key= "1e460c534dcd4c719f8233936222001";
+  localidade_nome : any;
+  forecast_day_number = 1;
+  aqi = "no";
+  alert = "no";
+  private url : any;
+
+  response: any;
+
+  constructor(private httpClient: HttpClient) { 
+
+    //this.getWeatherData();
+  }
+
+  doGetWeatherApi(localidade:string){
+    return this.httpClient.get(`http://api.weatherapi.com/v1/forecast.json?key=1e460c534dcd4c719f8233936222001&q=`+localidade+`&days=1&aqi=no&alerts=no`).toPromise();
+  
+  }
+
+  
+}
